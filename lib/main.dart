13bbb1bd5cfbbb0c89a6c696de835b75f1623fc5
nb923 <nb923@scarletmail.rutgers.edu>
2024-05-12 12:55:34 -0400

    Extract Start and End Textbox Widgets
    Implement outline for page for place autocomplete
    
    Revert "Implement outline for page for place autocomplete"
    
    This reverts commit 554d0541ba9c5b3436aeeec9c708abd372ddfef2.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-
+import 'package:http/http.dart' as http;
 void main() async {
   // Load Environment Variables
   await dotenv.load(fileName: '.env');
@@ -39,7 +41,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) 
   {
     return const Scaffold(
-      body: DirectionPage()
+      body: SearchBarPageState() // Change to Direction Page if you want to see the page with the maps on it
     );
   }
 }
@@ -55,19 +57,36 @@ class DirectionPage extends StatefulWidget {
 
 class _DirectionPageState extends State<DirectionPage> {
   late GoogleMapController mapController;
-  final LatLng _center = const LatLng(43.281631, -0.802300);
   final Set<Marker> _markers = {};
+
+  String startQuery = "";
+  String endQuery = "";
+
+  final LatLng _center = const LatLng(43.281631, -0.802300);
+
+  void setStartQuery(String s)
+  {
+    startQuery = s;
+  }
+
+  void setEndQuery(String s)
+  {
+    endQuery = s;
+  }
 
   @override
   Widget build(BuildContext context) {
+    LocationBar startLocationBar = LocationBar(callback: setStartQuery,);
+    LocationBar endLocationBar = LocationBar(callback: setEndQuery,);
+
     return Container(
       margin: const EdgeInsets.all(10),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.end,
         children: [
           googleMapWidget(),
-          startLocationBar(),
-          endLocationbar(),
+          startLocationBar,
+          endLocationBar,
         ],
       ),
     );
@@ -94,32 +113,103 @@ class _DirectionPageState extends State<DirectionPage> {
       _markers.add(Marker(markerId: MarkerId(markerId), position: l,)); 
     });
   }
+}
 
-  Widget startLocationBar()
+
+class LocationBar extends StatelessWidget {
+  final ValueChanged<String> callback;
+
+  const LocationBar({super.key, required this.callback});
+  
+  @override
+  Widget build(BuildContext context) {
+    return locationBar();
+  }
+
+  Widget locationBar()
   {
     return Container(
       margin: const EdgeInsets.all(10),
-      child: const TextField(
+      child: TextField(
         obscureText: false,
-        decoration: InputDecoration(
+        decoration: const InputDecoration(
           border: OutlineInputBorder(),
           labelText: 'Origin',
         ),
+        onChanged: (value)
+        {
+          callback(value);
+        },
+      ),
+    );
+  }
+}
+
+
+class SearchBarPageState extends StatefulWidget {
+  const SearchBarPageState({super.key});
+
+  @override
+  State<SearchBarPageState> createState() => _SearchBarPageState();
+}
+
+class _SearchBarPageState extends State<SearchBarPageState> {
+  String textInBar = "";
+  Map<String, String> httpAutocompletes = {};
+
+  void setTextInBar(String s)
+  {
+    setState(() {
+      textInBar = s;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(10),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          LocationBar(callback: setTextInBar,),
+        ],
       ),
     );
   }
 
-  Widget endLocationbar()
+  Container placesAutoComplete() // Does not work yet
   {
     return Container(
       margin: const EdgeInsets.all(10),
-      child: const TextField(
-        obscureText: false,
-        decoration: InputDecoration(
-          border: OutlineInputBorder(),
-          labelText: 'Destination',
-        ),
-      ),
+      child: ListView(
+        children: [
+          for (var entry in httpAutocompletes.entries)
+            ListTile(
+              leading: const Icon(Icons.favorite),
+            )
+        ],
+      )
     );
+  }
+
+  void fetchPlacesAutcomplete(String query, LatLng l) async // This method doesn't work completely yet, we need to set a timer to make the api requests slow down
+  {
+    final uri = Uri.parse("https://maps.googleapis.com/maps/api/place/autocomplete/json?input=$query&location=${l.latitude}%2C${l.longitude}&radius=500&key=${dotenv.env['MAPS_API_KEY']!}");
+    final response = await http.get(uri);
+    Map<String, String> m = {};
+
+    if (response.statusCode == 200)
+    {
+      final locations = jsonDecode(response.body);
+      locations['predictions'].forEach((value) => m[value['description']] = value['place_id']);
+
+      setState(() {
+        httpAutocompletes = m;
+      });
+    }
+    else
+    {
+      throw Exception('Failed to get places autocorrect');
+    }
   }
 }
